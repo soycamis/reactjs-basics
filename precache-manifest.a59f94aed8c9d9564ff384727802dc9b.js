65734897b398f28be8e54cb479f6a5dc90602a25
soycamis <camiilososa@gmail.com>
2020-08-03 15:09:42 -0500

    Updates

---
+++ precache-manifest.a59f94aed8c9d9564ff384727802dc9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d02b6a1a06c87bda5b3d4a0cdd3126a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a5dd86658e005af93fe",
+    "url": "/static/css/main.757e23cf.chunk.css"
+  },
+  {
+    "revision": "23a0a1d1b31ee273917f",
+    "url": "/static/js/2.2d8347f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2d8347f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a5dd86658e005af93fe",
+    "url": "/static/js/main.48302fd7.chunk.js"
+  },
+  {
+    "revision": "f7786b051f5dda308b1b",
+    "url": "/static/js/runtime-main.f6143d5a.js"
+  }
+]);
